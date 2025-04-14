7b5aeee330c2fe87c0320a37733d78c797a26c43
codeup123 <azkf@chcoo.cn>
2025-04-14 10:02:04 +0800

    refactory(route): update route /oauth/callback to /oauth/login

--- routes/oauth.js
+++ routes/oauth.js
@@ -15,7 +15,7 @@ var client_ids = [
     }
 ]
 
-router.get('/callback', async (req, res, next) => {
+router.get('/login', async (req, res, next) => {
     console.log(req.query);
     let { code, state, error } = req.query;
     let { client_id, client_secret, redirect_uri } = client_ids.find(item => item.client_id === 'ai-manager-client') || {};
