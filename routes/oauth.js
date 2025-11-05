3372af19ca03e6435e3f3f995347fb8e99e5a67b
hvail <41519593@qq.com>
2025-11-05 16:14:31 +0800

    add new token call back

--- routes/oauth.js
+++ routes/oauth.js
@@ -3,11 +3,19 @@ var router = express.Router();
 // var fetch = require('node-fetch');
 // import fetch from 'node-fetch';
 
+var authorize_host = 'https://oauth2.zshaojie.com';
+
 var client_ids = [
     {
         client_id: 'ai-manager-client-dev',
         client_secret: 'ai@choco',
-        redirect_uri: 'http://192.168.1./oauth/callback'
+        redirect_uri: 'http://localhost:8848/oauth/callback',
+        client_redirect_uri: 'http://localhost:8848/oauth/login'
+    },
+    {
+        client_id: 'oauth-express-simple-3',
+        client_secret: 'demo@2025',
+        redirect_uri: 'http://localhost:3000/oauth/callback'
     }, {
         client_id: 'ai-manager-client',
         client_secret: 'ai_choco',
@@ -15,10 +23,19 @@ var client_ids = [
     }
 ]
 
+router.get('/authorize', (req, res, next) => { 
+    let {state , client_id} = req.query;
+    console.log("authorize params:", req.query);
+    let { redirect_uri } = client_ids.find(item => item.client_id === client_id) || {};
+    res.redirect(`${authorize_host}/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=openid device`);``
+});
+
 router.get('/callback', async (req, res, next) => {
-    console.log(req.query);
+    // console.log(req.query);
     let { code, state, error } = req.query;
-    let { client_id, client_secret, redirect_uri } = client_ids.find(item => item.client_id === 'ai-manager-client') || {};
+    let { client_id, client_secret, redirect_uri, client_redirect_uri } = client_ids.find(item => item.client_id === 'ai-manager-client-dev') || {};
+    console.log("callback params:", req.query);
+    console.log("client info:", { client_id, redirect_uri });
     if (error) {
         console.log(error);
         return res.status(500).send({ "msg": error });
@@ -27,16 +44,23 @@ router.get('/callback', async (req, res, next) => {
         return res.status(500).send({ "msg": "no code" });
     }
 
-    let response = await fetch('https://oauth2.hunanxiaoya.com/oauth2/token', {
+    var requestOptions = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Basic ' + Buffer.from(`${client_id}:${client_secret}`).toString('base64')
         },
         body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}`
-    });
+    }
+    console.log("requestOptions", requestOptions);
+    let response = await fetch(`${authorize_host}/oauth2/token`, requestOptions);
+    console.log(response);
     let data = await response.json();
-    res.render('oauth', { data: JSON.stringify(data) });
+    console.log(data);
+    let {access_token, id_token, refresh_token, token_type, expires_in, scope} = data;
+    res.redirect(`${client_redirect_uri}?access_token=${access_token}&id_token=${id_token}&scope=${scope}`);
+
+    // res.render('oauth', { msg: JSON.stringify(data), data: data });
 });
 
 module.exports = router;
