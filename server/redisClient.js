cfe9f3a5fd599c24f405c05d027e33c16874f7ad
hvail <41519593@qq.com>
2025-11-06 14:51:45 +0800

    fix: update the redis config

--- server/redisClient.js
+++ server/redisClient.js
@@ -5,8 +5,8 @@ class RedisClient {
     constructor(options = {}) {
         this.client = createClient({
             url: options.url || process.env.REDIS_URL || 'redis://localhost:6379',
-            password: options.password || process.env.REDIS_PASSWORD,
-            username: options.username || process.env.REDIS_USERNAME,
+            password: options.password || process.env.REDIS_PASSWORD || '',
+            username: options.username || process.env.REDIS_USERNAME || '',
             database: typeof options.database === 'number' ? 
                 options.database : 
                 (options.db || parseInt(process.env.REDIS_DATABASE) || 0),
